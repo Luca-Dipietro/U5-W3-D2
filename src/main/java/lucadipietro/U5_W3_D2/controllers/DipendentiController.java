348ddce2d29fe960d455da409a7798ce5f0895a8
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-07-02 16:53:40 +0200

    Autenticazione fatta

--- src/main/java/lucadipietro/U5_W3_D2/controllers/DipendentiController.java
+++ src/main/java/lucadipietro/U5_W3_D2/controllers/DipendentiController.java
@@ -7,6 +7,8 @@ import lucadipietro.U5_W3_D2.services.DipendentiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -26,12 +28,34 @@ public class DipendentiController {
         return this.dipendentiService.getDipendenti(page,size,sortBy);
     }
 
+    @GetMapping("/me")
+    public Dipendente getProfile(@AuthenticationPrincipal Dipendente currentAuthenticatedDipendente){
+        return currentAuthenticatedDipendente;
+    }
+
+    @PutMapping("/me")
+    public Dipendente updateProfile(@AuthenticationPrincipal Dipendente currentAuthenticatedDipendente, @RequestBody DipendentiDTO body){
+        return this.dipendentiService.findByIdAndUpdate(currentAuthenticatedDipendente.getId(), body);
+    }
+
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteProfile(@AuthenticationPrincipal Dipendente currentAuthenticatedDipendente){
+        this.dipendentiService.findByIdAndDelete(currentAuthenticatedDipendente.getId());
+    }
+
+    @PatchMapping("/me/avatar")
+    public Dipendente updateAvatar(@PathVariable UUID dipendenteId, @RequestParam("avatar") MultipartFile image) throws IOException {
+        return this.dipendentiService.uploadImage(dipendenteId, image);
+    }
+
     @GetMapping("/{dipendenteId}")
     public Dipendente findById(@PathVariable UUID dipendenteId){
         return this.dipendentiService.findById(dipendenteId);
     }
 
     @PutMapping("/{dipendenteId}")
+    @PreAuthorize("hasAuthority('CAPO_REPARTO')")
     public Dipendente findByIdAndUpdate(@PathVariable UUID dipendenteId,@RequestBody @Validated DipendentiDTO body,BindingResult validationResult){
         if(validationResult.hasErrors()){
             throw new BadRequestException(validationResult.getAllErrors());
@@ -42,11 +66,13 @@ public class DipendentiController {
 
     @DeleteMapping("/{dipendenteId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PreAuthorize("hasAuthority('CAPO_REPARTO')")
     public void findByIdAndDelete(@PathVariable UUID dipendenteId){
         this.dipendentiService.findByIdAndDelete(dipendenteId);
     }
 
     @PatchMapping("/{dipendenteId}/avatar")
+    @PreAuthorize("hasAuthority('CAPO_REPARTO')")
     public Dipendente uploadAvatar(@PathVariable UUID dipendenteId, @RequestParam("avatar") MultipartFile image) throws IOException {
         return this.dipendentiService.uploadImage(dipendenteId, image);
     }
